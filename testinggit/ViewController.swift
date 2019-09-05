cf90288bffbb4ebe8b557206ea87aba7fb1a4260
David Jewell <s3633028@student.rmit.edu.au>
2019-09-05 20:29:19 +0800

    commiting

--- testinggit/ViewController.swift
+++ testinggit/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
        let text=mainButton.titleLabel?.text
         print (text)
+        let output=text
+        print (output)
 
     }
 
